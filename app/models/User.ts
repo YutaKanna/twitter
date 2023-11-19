3a7db906519e796306f92c249a9888cb2775d33a
YutaKanna <kannayuta0629@gmail.com>
2023-11-19 20:16:09 +0900

    修正

--- app/models/User.ts
+++ app/models/User.ts
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose';
+import mongoose, { CallbackError } from 'mongoose';
 import bcrypt from 'bcrypt';
 
 const userSchema = new mongoose.Schema({
@@ -13,11 +13,11 @@ userSchema.pre('save', async function (next) {
     if (typeof this.password === 'string') {
       this.password = await bcrypt.hash(this.password, 10);
     } else {
-      // パスワードがstring型でない場合のエラーハンドリング
       throw new Error('Password must be a string');
     }
   } catch (error) {
-    return next(error);
+    // 'error' を 'CallbackError' として扱う
+    return next(error as CallbackError);
   }
 
   next();
