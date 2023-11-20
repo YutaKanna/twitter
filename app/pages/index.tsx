b5f97bcd406ebf01dd426ee035c52cdddf855d9d
YutaKanna <kannayuta0629@gmail.com>
2023-11-20 20:18:24 +0900

    fix

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -113,7 +113,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" integrity="sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A==" crossOrigin="anonymous" referrerpolicy="no-referrer" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" integrity="sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
       </Head>
       <div className="container mx-auto h-screen flex xl:max-w-[1200px]">
         {/* 左側のコンポーネント */}
